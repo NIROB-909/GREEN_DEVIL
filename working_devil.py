33f97b95d0171bbda656d23231008036886f033c
NIROB SHEIKH <154347282+NIROB-909@users.noreply.github.com>
2024-01-07 22:16:52 +0600

    Create working_devil.py

---
+++ working_devil.py
@@ -0,0 +1,8 @@
+#খানকির পোলা পারলে ফাইল চুরি কর 
+#তর পাপা NIROB SHEIKH মাদারবোর্ড
+#তর টিম কে আন্তর্জাতিক ভাবে চুদে 
+#Hello Hacker,s 0.2
+
+import os
+import working_devil.py
+working_devil.py.loging()
